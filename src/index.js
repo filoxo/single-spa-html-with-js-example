ce847d41627f1309a5669caea697e8de70b4adc7
Carlos Filoteo <c.filoteo12@gmail.com>
2020-05-28 09:25:52 -0600

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import singleSpaHtml from "single-spa-html";
-import template from "./template.html";
-import "./styles.css";
+import singleSpaHtml from "single-spa-html"; // single-spa helper
+import template from "./template.html"; // html template is separated out so that we can get better syntax highlighting
+import "./styles.css"; // styles are global so these are based on IDs
 
 const htmlLifecycles = singleSpaHtml({
   domElementGetter: () => {
@@ -9,7 +9,7 @@ const htmlLifecycles = singleSpaHtml({
     if (!el) {
       el = document.createElement("div");
       el.id = id;
-      document.body.prepend(el);
+      document.body.prepend(el); // single-spa automatically _appends_, but this content should be _prepended_ for accessibility
     }
     return el;
   },
@@ -17,28 +17,29 @@ const htmlLifecycles = singleSpaHtml({
 });
 
 export const mount = (props) => {
-  return localStorage.getItem("cookie-consent")
-    ? Promise.resolve(null)
-    : htmlLifecycles.mount(props).then(() => {
-        const dialog = document.querySelector(`#cookie-consent`),
-          noSellCheckbox = dialog.querySelector("#cookie-consent-no-sell"),
-          acceptBtn = dialog.querySelector("#cookie-consent-accept");
+  return localStorage.getItem("cookie-consent") // look for value; this could instead be a cookie if you wanted to send it back and forth to your server.
+    ? Promise.resolve(null) // don't render anything if they have already "consented"
+    : htmlLifecycles.mount(props).then(() => { // extend single-spa mount lifecycle; after single-spa has mounted the template, enhance with plain JavaScript
+        const dialog = document.querySelector(`#cookie-consent`), // get outermost node
+          noSellCheckbox = dialog.querySelector("#cookie-consent-no-sell"), // get checkbox node
+          acceptBtn = dialog.querySelector("#cookie-consent-accept"); // get button node
 
-        acceptBtn.addEventListener("click", () => {
+        acceptBtn.addEventListener("click", () => { // bind and handle click event on button
           const consent = {
             date: new Date().toJSON(),
             noSell: noSellCheckbox.checked,
           };
           localStorage.setItem("cookie-consent", JSON.stringify(consent));
-          dialog.classList.add("h");
+          dialog.classList.add("h"); // add hidden class to animate out
         });
 
-        setTimeout(() => dialog.classList.remove("h"));
+        setTimeout(() => dialog.classList.remove("h")); // dialog starts out with 'h' (hidden) class; this removes it so that it animates in.
 
         dialog.addEventListener("transitionend", () => {
+          // listen for when animation ends and set hidden attribute
           dialog.classList.contains('h') && dialog.setAttribute("hidden", "");
         });
       });
 };
 
-export const { bootstrap, unmount } = htmlLifecycles;
+export const { bootstrap, unmount } = htmlLifecycles; // export other lifecycles as-is
