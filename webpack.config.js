47f72fa174e1de9dfd6dba73218fec2b7a0ec908
Carlos Filoteo <c.filoteo12@gmail.com>
2022-03-31 03:59:09 -0600

    Fix onListening, format

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
-const path = require('path');
-const packageJson = require('./package.json');
+const path = require("path");
+const packageJson = require("./package.json");
+
+const isAnyOf = (value, list) => list.includes(value);
 
 module.exports = (opts) => {
   let isProduction =
-    opts.argv && (opts.argv.p || opts.argv.mode === 'production');
+    opts.argv && (opts.argv.p || opts.argv.mode === "production");
 
   return {
-    mode: isProduction ? 'production' : 'development',
+    mode: isProduction ? "production" : "development",
     output: {
       filename: `index.js`,
-      libraryTarget: 'system',
-      path: path.resolve(process.cwd(), 'dist'),
+      libraryTarget: "system",
+      path: path.resolve(process.cwd(), "dist"),
       uniqueName: packageJson.name,
-      publicPath: '',
+      publicPath: "",
     },
     module: {
       rules: [
@@ -25,52 +27,62 @@ module.exports = (opts) => {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
           },
         },
         {
           test: /\.html$/i,
-          use: ['html-loader'],
+          use: ["html-loader"],
         },
         {
           test: /\.css$/i,
           use: [
-            'style-loader',
+            "style-loader",
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
                 importLoaders: 1,
+                modules: true,
               },
             },
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
             },
           ],
         },
       ],
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     devServer: {
       compress: true,
       historyApiFallback: true,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
       },
-      firewall: false,
-      client: {
-        host: 'localhost',
-      },
-      onListening: ({ compiler }) => {
-        const { https, client } = compiler.options.devServer;
+      onListening: function ({
+        server /* https://nodejs.org/api/net.html#class-netserver */,
+        compiler,
+      }) {
+        if (!server)
+          throw new Error("webpack-dev-server is missing a server instance");
+
+        // config values
+        const { port: serverPort, address } = server.address();
         const { publicPath, filename } = compiler.options.output;
-        const protocol = https ? 'https://' : 'http://';
-        const port = !!client.port ? `:${client.port}` : '';
-        const path = ['', 'auto'].includes(publicPath) ? '/' : publicPath;
+
+        // derived values
+        const protocol = compiler.options.devServer.https
+          ? "https://"
+          : "http://";
+        const host = address === "::" ? "localhost" : address;
+        const port = Boolean(serverPort) ? `:${serverPort}` : "";
+        const path = isAnyOf(publicPath, ["", "auto"]) ? "/" : publicPath;
+
         console.log(
-          `⚡️ single-spa application entry: ${protocol}${client.host}${port}${path}${filename}`
+          `\n  ⚡️ single-spa application entry: ${protocol}${host}${port}${path}${filename}\n`
         );
       },
     },
-    externals: ['single-spa'],
+    externals: ["single-spa"],
   };
 };
